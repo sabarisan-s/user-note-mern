93b266863d2dee9479bb2995f7f0f57b10117964
sabarisan-s <iamsabarisan@gmail.com>
2024-05-01 18:13:07 +0530

    updated url

--- frontend/notes-app/src/utils/axiosInstance.js
+++ frontend/notes-app/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = "https://notes-mern-api.onrender.com/";
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
