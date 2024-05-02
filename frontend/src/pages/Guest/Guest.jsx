9f35fb824f18323a27be381c28e0de755cfb7724
sabarisan-s <iamsabarisan@gmail.com>
2024-05-02 11:23:44 +0530

    updated content

--- frontend/src/pages/Guest/Guest.jsx
+++ frontend/src/pages/Guest/Guest.jsx
@@ -14,7 +14,7 @@ const Guest = () => {
             />
 
             <p className="text-center mx-auto animate-bounce ">
-                Guest page soon ready! Please experience as User (register/login) page... Page little bit slow because of render deploy slow api 59s   
+                Guest page soon ready! Please experience as User (register/login) page... Page little bit slow render deploy slow api 59s   
             </p>
             </div>
         </div>
